d8d16048cea28766971c3a8067a79c0449650dda
Ben Camp <tuf73864@temple.edu>
2017-01-07 20:56:11 -0500

    Many thoughts
    Spent a lot of time thinking about what kind of classes I should use and
    went through several iterations.

---
+++ Unity Projects/SpaceShipAssembly2D/Assets/GameLoop.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameLoop : MonoBehaviour {
+	Dictionary<int, Structure> structures = new Dictionary<int, Structure>();
+	Dictionary<int, Wall> walls = new Dictionary<int, Wall>();
+	Dictionary<int, Item> items = new Dictionary<int, Item>();
+
+	public void addStructure (Structure structure){
+		structures.Add (structure.GetInstanceID(), structure);
+	}
+
+	public Structure getStructure (int id){
+		return structures [id];
+	}
+
+	public void addWall (Wall wall){
+		walls.Add (wall.GetInstanceID(), wall);
+	}
+
+	public Wall getWall(int id){
+		return walls [id];
+	}
+
+	public void addItems (Item item){
+		items.Add (item.GetInstanceID(), item);
+	}
+
+	public Item getItem (int id){
+		return items [id];
+	}
+
+
+}

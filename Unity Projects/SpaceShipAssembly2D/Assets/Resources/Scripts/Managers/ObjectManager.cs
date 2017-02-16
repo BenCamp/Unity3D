44e15f56fa511fe5362613ed7149068b867f1a40
BenCamp <benjaminjamescamp@gmail.com>
2017-02-16 10:00:54 -0500

    Forgot to backup for  a while now. Mostly been backtracking and rearranging, trying to find better ways of organization. Also working on making non structure objects able to be attached.

--- Unity Projects/SpaceShipAssembly2D/Assets/Resources/Scripts/Managers/ObjectManager.cs
+++ Unity Projects/SpaceShipAssembly2D/Assets/Resources/Scripts/Managers/ObjectManager.cs
@@ -2,6 +2,36 @@
 using System.Collections;
 
 abstract public class ObjectManager : MonoBehaviour {
-	private bool isSelectable;
+	protected ShipManager shipManager;
+	private bool isSelectable = false;
 	public bool IsSelectable { get { return isSelectable; } set { isSelectable = value; } }
+
+	private bool isPlaceable = false;
+	public bool IsPlaceable { get { return isPlaceable; } set { isPlaceable = value; } }
+
+	protected string objectName;
+
+
+	public GameObject SpawnPlaceable (){
+		GameObject spawn = 
+			Instantiate (
+				Resources.Load (
+					"Prefabs/PlacementClonePrefabs/"
+					+ objectName
+					+ "Clone"), 
+				Vector3.zero, 
+				Quaternion.identity) 
+			as GameObject;
+		spawn.GetComponent <CloneManager> ().Spawner = gameObject;
+		return spawn;
+	}
+
+	public void SetShip (ShipManager given){
+		shipManager = given;
+	}
+
+	public virtual bool CanThisBePlaced () {
+		return true;
+	}
+	abstract public void StateChange ();
 }

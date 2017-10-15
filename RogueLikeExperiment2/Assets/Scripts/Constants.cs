44ceb5c8f9d6c9f802b4b90ccf5fe5c1121a1fbb
BenCamp <benjaminjamescamp@gmail.com>
2017-10-15 15:05:08 -0400

    Damn locking

---
+++ RogueLikeExperiment2/Assets/Scripts/Constants.cs
@@ -0,0 +1,18 @@
+﻿public static class Constants {
+	public const int PIXELSIZE = 1;
+	public const int PLAYERHEIGHT = 64;
+	public const int MAXHEIGHT = 1000 * PIXELSIZE;
+	public const int MINHEIGHT = 500 * PIXELSIZE;
+	public const int MAXWIDTH = 1000 * PIXELSIZE;
+	public const int MINWIDTH = 500 * PIXELSIZE;
+
+	public const int MAXROOMWIDTH = 200 * PIXELSIZE;
+	public const int MINROOMWIDTH = 100 * PIXELSIZE;
+	public const int MAXROOMHEIGHT  = 200 * PIXELSIZE;
+	public const int MINROOMHEIGHT = 100 * PIXELSIZE;
+	public const int MAXTRIES = 50;
+	public const int MAXROOMS = 9;
+
+
+
+}
---
+++ RogueLikeExperiment2/Assets/Scripts/ShowTerrain.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ClipperLib;
+using VecPath = System.Collections.Generic.List<UnityEngine.Vector2>;
+using VecPaths = System.Collections.Generic.List<System.Collections.Generic.List<UnityEngine.Vector2>>;
+using Path = System.Collections.Generic.List<ClipperLib.IntPoint>;
+using Paths = System.Collections.Generic.List<System.Collections.Generic.List<ClipperLib.IntPoint>>;
+
+public class ShowTerrain : MonoBehaviour{
+
+	SpriteRenderer sr;
+	PolygonCollider2D pc;
+	Texture2D texture;
+	Color clear = new Color (0,0,0,0);
+	Color terrain = new Color (.12f, .59f, .7f, 1);
+	Color marker = new Color (1, 0, 0, 1);
+	// Use this for initialization
+	void Start () {
+		pc = gameObject.GetComponent<PolygonCollider2D> ();
+		sr = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	public void UpdateTerrainImage (int width, int height, Paths paths, int pixelSize) { 
+		texture = new Texture2D(width, height);
+		Color[] colors = new Color[width * height];
+		for (int i = 0; i < height; i++) {
+			
+			for (int j = 0; j < width; j++){
+				if (pc.OverlapPoint (new Vector2 (j - width / 2, i - height / 2))) {
+					colors [i * width + j] = terrain;
+				}
+				else {
+					colors [i * width + j] = clear;
+				}
+			}
+		}
+
+		texture.SetPixels (colors);
+		texture.alphaIsTransparency = true;
+
+		texture.Apply();
+
+		sr.sprite = Sprite.Create (texture, new Rect (new Vector2 (0, 0), new Vector2 (width, height)), new Vector2 (.5f, .5f), 1f);
+	}
+}
